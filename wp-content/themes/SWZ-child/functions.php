8db8bc2a9833bc8f2fadb65ae0ad684d92e1d9df
hamyvosugh <online@breadcrumb.de>
2024-12-23 12:23:30 +0100

    SEO added

--- wp-content/themes/SWZ-child/functions.php
+++ wp-content/themes/SWZ-child/functions.php
@@ -34,3 +34,43 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+
+/** SEO */
+
+function swz_add_seo_meta_tags() {
+    ?>
+    <!-- Dynamic Title Tag (Only if your theme doesn't automatically handle it) -->
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed urna in justo euismod condimentum.">
+    <meta name="Breadcrumb" content="Sportwagen Zentrum">
+
+    <!-- Open Graph / Facebook Meta Tags for rich previews in social sharing -->
+    <meta property="og:title" content="<?php echo get_the_title(); ?> | Lorem Ipsum ">
+    <meta property="og:description" content="<?php echo get_the_excerpt(): 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed urna in justo euismod condimentum.'; ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?php echo get_permalink(); ?>">
+    <meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/path/to/your/default-og-image.jpg">
+    <meta property="og:site_name" content="Sportwagen Zentrum">
+    <meta property="og:locale" content="de_DE">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="<?php echo get_the_title(); ?>">
+    <meta name="twitter:description" content="<?php echo is_single() || is_page() ? get_the_excerpt() : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed urna in justo euismod condimentum.'; ?>">
+    <meta name="twitter:image" content="<?php echo get_stylesheet_directory_uri(); ?>/path/to/your/default-twitter-image.jpg">
+    <meta name="twitter:site" content="@YourTwitterHandle">
+    <meta name="twitter:creator" content="@YourTwitterHandle">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/site.webmanifest">
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon.ico">
+    <meta name="theme-color" content="#ffffff">
+    <?php
+}
+add_action('wp_head', 'swz_add_seo_meta_tags');
