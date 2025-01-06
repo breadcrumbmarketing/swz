515bdc90d72360ac021dd9c8b169b55e63222d60
hamyvosugh <online@breadcrumb.de>
2025-01-06 15:12:55 +0100

    asdfasd

--- wp-content/themes/SWZ-child/gallery-template.php
+++ wp-content/themes/SWZ-child/gallery-template.php
@@ -214,7 +214,10 @@ body, .filter-bar select, .filter-bar button, .gallery-card h3, .gallery-card p 
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   color: white;
 }
-
+.clear-filter-button 
+{
+    color: white;
+}
 
 
 
