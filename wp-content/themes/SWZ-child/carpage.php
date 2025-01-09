dfd3487bb138f26187f7ad03a57c99e8060549e9
hamyvosugh <online@breadcrumb.de>
2025-01-09 16:46:30 +0100

    ASD

--- wp-content/themes/SWZ-child/carpage.php
+++ wp-content/themes/SWZ-child/carpage.php
@@ -257,11 +257,15 @@ footer {
 .edit-bgcolor {
           
         font-family: 'Poppins', sans-serif !important;
-        font-weight: 400 !important;
+        font-weight: 600 !important;
         text-align: justify !important;
         padding-top: 20px !important;
-        font-size: 25px !important;
+        font-size: 32px !important;
        }
+
+.themeBtn-icon-wrapper {
+    background-color: #DE4F3E  !important;
+}      
 </style>
 </head>
 <body>
