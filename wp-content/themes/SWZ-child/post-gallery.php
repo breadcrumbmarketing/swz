eca47e5149d20b3b603dd768add1a54383a4036f
hamyvosugh <online@breadcrumb.de>
2025-01-13 18:53:31 +0100

    asdas

--- wp-content/themes/SWZ-child/post-gallery.php
+++ wp-content/themes/SWZ-child/post-gallery.php
@@ -106,7 +106,7 @@ body, .filter-bar select, .filter-bar button, .gallery-card h3, .gallery-card p 
     overflow: hidden;
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
     transition: transform 0.3s ease, box-shadow 0.3s ease;
-    height: 500px; /* Ensures all cards are the same height */
+    height: 450px; /* Ensures all cards are the same height */
     text-decoration: none; /* Remove link styles */
 }
 
