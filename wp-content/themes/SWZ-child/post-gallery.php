6d456fa8b9a18c1120968c91f46380b5bf323012
hamyvosugh <online@breadcrumb.de>
2025-01-13 19:20:56 +0100

    sdf

--- wp-content/themes/SWZ-child/post-gallery.php
+++ wp-content/themes/SWZ-child/post-gallery.php
@@ -401,7 +401,7 @@ body, .filter-bar select, .filter-bar button, .gallery-card h3, .gallery-card p 
                   
 
                     <!-- Post Title -->
-                    <h2><?php the_title(); ?></h2>
+                    <h3><?php the_title(); ?></h3>
 
                     <!-- Excerpt -->
                     <p class="excerpt">
